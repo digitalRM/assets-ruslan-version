3f9910135d1cdf10c3dd13214754eec1b8823f6d
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2023-07-04 10:26:08 -0700

    Added basic functionality

--- mrb.js
+++ mrb.js
@@ -7,7 +7,6 @@ async function renderText(title, chapter) {
     let info = await fetchText()
     let data = info[title][chapter]
     let html = ""
-    console.log(data);
     data.forEach((passage, i) => {
         let segment = `<p><span class="number">${i+1}</span>
                     ${passage}
@@ -24,11 +23,11 @@ renderText("Genesis", 1)
 document.getElementById("button").addEventListener("click", () => {
     if (chapter == 1) {
         renderText("Genesis", 2)
-        document.getElementById("button").innerHTML = `<i class="fa-solid fa-arrow-left"></i>`
+        document.getElementById("button").innerHTML = `<i class="fa-solid fa-arrow-left large"></i>`
         chapter = 2
     } else {
         renderText("Genesis", 1)
-        document.getElementById("button").innerHTML = `<i class="fa-solid fa-arrow-right"></i>`
+        document.getElementById("button").innerHTML = `<i class="fa-solid fa-arrow-right large"></i>`
         chapter = 1
     }
     document.getElementById("chapter").innerHTML = `${chapter}`
