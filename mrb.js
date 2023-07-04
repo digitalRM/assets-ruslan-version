25f94bb114a78e323a49ebf6ebd6061c1a09dd19
Zhu <57535849+DerekZZhu@users.noreply.github.com>
2023-07-03 21:01:04 -0700

    Updated json file

--- mrb.js
+++ mrb.js
@@ -1,3 +1,23 @@
-fetch('https://server.com/data.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+async function fetchText() {
+    let res = await fetch("https://raw.githubusercontent.com/DerekZZhu/Assets/main/mukrobbible.json")
+    return await res.json()
+}
+
+async function renderText(title, chapter) {
+    let info = await fetchText()
+    let data = info[title][chapter]
+    let html = ""
+    console.log(data);
+    data.forEach((passage, i) => {
+        console.log(passage);
+        let segment = `<p><span class="number">${i+1}</span>
+                    ${passage}
+                    </p>`
+        html += segment
+    })
+
+    let container = document.querySelector(".passages")
+    container.innerHTML = html;
+}
+
+renderText("Genesis", 1)
